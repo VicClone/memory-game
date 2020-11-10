004126b8bb8a85dc8ca0129ec69b3301ffcc749c
Serg <36944165+bizzquit@users.noreply.github.com>
2020-11-11 00:03:06 +0500

    сделал формирование 20 карт, рандомным orderÐом перемешал, открывает 2 карты - если разные - закрывает

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-class CardRender {
+const board = document.getElementById("game-board");
 
+class CardRender {
   card = createFromTemplate();
 
   setImgFront(image) {
-    const frontImg = this.card.querySelector('.memory-game__card-front');
+    const frontImg = this.card.querySelector(".memory-game__card-front");
     frontImg.src = `src/${image}`;
   }
 }
@@ -19,34 +20,82 @@ class Card {
   card = this.render.card;
 
   flip() {
-    this.card.classList.toggle('flip');
-    this.fliped = !this.fliped;
+    if (!this.fliped && game.openCards.length < 2) {
+      this.card.classList.add("flip");
+      this.fliped = !this.fliped;
+      game.openCards.push(this);
+    }
+    if (game.openCards.length === 2) {
+      game.compareCards();
+    }
   }
 
   create(board) {
     this.render.setImgFront(this.img);
-    board.appendChild(this.render.card);
+    board.appendChild(this.card);
 
     this.card.onclick = () => {
       this.flip();
+    };
+  }
+}
+
+class Game {
+  constructor() {
+    this.cards = [];
+    this.openCards = [];
+    this.cardsInGame = 10;
+  }
+
+  compareCards() {
+    if (this.openCards[0].name !== this.openCards[1].name) {
+      return setTimeout(() => {
+        flipBack.call(this);
+      }, 500);
     }
   }
 
+  start() {
+    generatingMaps.call(this);
+  }
 }
 
 function createFromTemplate() {
-  const cardTemplate = document.getElementById('card-template');
+  const cardTemplate = document.getElementById("card-template");
   const cardClone = cardTemplate.cloneNode(true);
-  cardClone.removeAttribute('id');
+  cardClone.removeAttribute("id");
+  const orderForCard = randomOrder(20);
+  cardClone.setAttribute("style", `order:${orderForCard}`);
 
   return cardClone;
 }
 
+function generatingMaps() {
+  for (let i = 1; i <= this.cardsInGame; i++) {
+    let y = 0;
+    do {
+      const card = new Card(i, `${i}.png`);
+      this.cards.push(card);
+      card.create(board);
+      y++;
+    } while (y < 2);
+  }
+  console.log(this.cards);
+}
 
-const board = document.getElementById('game-board');
+function flipBack() {
+  for (let item of this.openCards) {
+    item.card.classList.remove("flip");
+    item.fliped = !item.fliped;
+  }
+  this.openCards.length = 0;
+}
 
-const cardCat = new Card('cat', 'cat.png');
-cardCat.create(board);
+const randomOrder = (max) => {
+  const min = 0;
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
-const cardDog = new Card('dog', 'dog.png');
-cardDog.create(board);
+const game = new Game();
+game.start();
