96d68c56bfbab492fb348520f2a82fccea315a6b
Serg <36944165+bizzquit@users.noreply.github.com>
2020-11-13 13:07:58 +0500

    привел пÐпроект к послеÐмаленÐькие корректировки

--- src/modules/Game.js
+++ src/modules/Game.js
@@ -1,9 +1,10 @@
-import { generatingMaps, flipBack } from "./Helpers.js";
+import { generatingMaps, flipBack, removeCards } from "./Helpers.js";
 
 export default class Game {
   constructor() {
     this.cards = [];
     this.openCards = [];
+    this.guessedCards = [];
     this.pairsInGame = 10;
   }
 
@@ -21,8 +22,14 @@ export default class Game {
     if (this.openCards[0].name !== this.openCards[1].name) {
       return setTimeout(() => {
         flipBack.call(this);
-      }, 500);
+      }, 1000);
     }
+    return setTimeout(() => {
+      this.guessedCards.push(...this.openCards);
+      this.cards.length === this.guessedCards.length
+        ? console.log("Конец")
+        : removeCards.call(this);
+    }, 1000);
   }
 
   start() {
--- src/modules/Helpers.js
+++ src/modules/Helpers.js
@@ -4,7 +4,17 @@ const board = document.getElementById("game-board");
 
 function generatingMaps() {
   for (let i = 1; i <= this.pairsInGame; i++) {
-    createTwoCards.call(this);
+    let y = 0;
+    const randomIdx = uniqueNum(this.cards);
+    do {
+      const card = new Card(randomIdx, `${randomIdx}.png`);
+      this.cards.push(card);
+      card.create(board);
+      card.card.onclick = () => {
+        this.flipping(card);
+      };
+      y++;
+    } while (y < 2);
   }
 }
 
@@ -21,28 +31,21 @@ function flipBack() {
   this.openCards.length = 0;
 }
 
-function createTwoCards() {
-  let y = 0;
-  const randomIdx = uniqueNum(this.cards);
-  do {
-    const card = new Card(randomIdx, `${randomIdx}.png`);
-    this.cards.push(card);
-    card.create(board);
-    card.card.onclick = () => {
-      this.flipping(card);
-    };
-    y++;
-  } while (y < 2);
+function removeCards() {
+  for (let item of this.openCards) {
+    item.card.classList.remove("active");
+    item.active = !item.active;
+  }
+  this.openCards.length = 0;
 }
 
 function uniqueNum(array) {
   const num = randomOrder(1, 45);
   const check = array.filter((item) => item.name === num);
   if (check.length) {
-    uniqueNum(array);
+    return uniqueNum(array);
   }
-  console.log(array);
   return num;
 }
 
-export { generatingMaps, randomOrder, flipBack };
+export { generatingMaps, randomOrder, flipBack, removeCards };
