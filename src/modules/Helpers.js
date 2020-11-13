d51d28a352eb14de15dc9dbb834831085e122a28
Serg <36944165+bizzquit@users.noreply.github.com>
2020-11-13 15:18:53 +0500

    small corrects

--- src/modules/Helpers.js
+++ src/modules/Helpers.js
@@ -61,7 +61,7 @@ function uniqueNum(array) {
 function getRandomColor() {
   const letters = "0123456789ABCDEF";
   let color = "#";
-  for (var i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
