4b039fd996f969d5af3b67ca778f98b8841dac0c
Serg <36944165+bizzquit@users.noreply.github.com>
2020-12-03 19:29:15 +0500

    misprint in leaderboard

--- src/modules/Leaderboard.js
+++ src/modules/Leaderboard.js
@@ -1,19 +1,19 @@
-export default class Leaderboald {
+export default class Leaderboard {
   async write(userName, score, level) {
     const player = {
       name: userName,
       score: score,
       level: level,
-    }
+    };
 
-    fetch('/leaderboard', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(player)
-      })
-      .then(response => console.log(response))
-      .catch(error => console.error(error));
+    fetch("/leaderboard", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(player),
+    })
+      .then((response) => console.log(response))
+      .catch((error) => console.error(error));
   }
 }
